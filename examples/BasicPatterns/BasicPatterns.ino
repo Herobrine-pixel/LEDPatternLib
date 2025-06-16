8df29e484236d74a237282f33f55d4d9a612c1c5
Herobrine Sah <ashasah77@gmail.com>
2025-06-16 12:11:17 +0530

    Update BasicPatterns.ino

--- examples/BasicPatterns/BasicPatterns.ino
+++ examples/BasicPatterns/BasicPatterns.ino
@@ -1,6 +1,10 @@
 #include <LEDPatternLib.h>
 
-LEDPatternLib led(6);
+// Use this for a NeoPixel strip
+// LEDPatternLib led(6, 8);  // Pin 6, 8 NeoPixel LEDs
+
+// Use this for a simple built-in LED
+LEDPatternLib led(13);
 
 void setup() {
   led.begin();
@@ -8,5 +12,10 @@ void setup() {
 }
 
 void loop() {
-  led.rainbowCycle();
+  // For NeoPixel:
+  // led.rainbowCycle(10);
+  // led.knightRider(0xFF0000, 70);
+  
+  // For built-in LED:
+  led.blink(0xFFFFFF, 500);
 }
