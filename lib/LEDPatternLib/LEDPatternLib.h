6cbe29ce6e8f3f288e83fcb498a40698641ac587
Herobrine Sah <ashasah77@gmail.com>
2025-06-16 11:55:17 +0530

    Update LEDPatternLib.h

--- lib/LEDPatternLib/LEDPatternLib.h
+++ lib/LEDPatternLib/LEDPatternLib.h
@@ -1,21 +1,21 @@
-#ifndef LEDPatternLib_h
-#define LEDPatternLib_h
+#ifndef LED_PATTERN_LIB_H
+#define LED_PATTERN_LIB_H
 
+#include <Adafruit_NeoPixel.h>
 #include <Arduino.h>
 
 class LEDPatternLib {
-  public:
-    LEDPatternLib(uint8_t pin);
+public:
+    LEDPatternLib(uint8_t pin, uint16_t numPixels);
     void begin();
-    void blink(unsigned long delay_ms = 500);
-    void rainbowCycle(uint8_t wait = 10);
-    void knightRider(uint8_t speed = 50);
-    void colorWipe(uint32_t color, int delay_ms = 50);
+    void setBrightness(uint8_t brightness);
+    void blink(uint32_t color, uint16_t delay_ms = 500);
+    void rainbowCycle(uint8_t wait = 20);
+    void knightRider(uint32_t color = 0xFF0000, uint8_t speed = 50);
 
-  private:
-    uint8_t _pin;
-    uint32_t Wheel(byte WheelPos);
-    void showColor(uint32_t color);
+private:
+    Adafruit_NeoPixel _strip;
+    uint16_t _numPixels;
 };
 
 #endif
